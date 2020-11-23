ee4a5acc506cddee5bf1e42cab9663052340c2a8
Mike Cann <mike.cann@gmail.com>
2020-11-23 20:26:01 +0800

    wild splat

--- next.config.js
+++ next.config.js
@@ -2,9 +2,9 @@ module.exports = {
   async rewrites() {
     return [
       {
-        source: '/wp-content/:splat',
-        destination: 'http://d18l99bmg6trdn.cloudfront.net/wp-content/:splat',
+        source: "/wp-content/:splat*",
+        destination: "http://d18l99bmg6trdn.cloudfront.net/wp-content/:splat*",
       },
-    ]
+    ];
   },
-}
+};
