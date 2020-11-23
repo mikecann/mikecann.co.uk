79327ac339007259e34114d67705c03831cc4973
Mike Cann <mike.cann@gmail.com>
2020-11-23 20:24:20 +0800

    added some rewrites

---
+++ next.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  async rewrites() {
+    return [
+      {
+        source: '/wp-content/:splat',
+        destination: 'http://d18l99bmg6trdn.cloudfront.net/wp-content/:splat',
+      },
+    ]
+  },
+}
