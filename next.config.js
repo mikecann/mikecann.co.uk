3997807562ecac44c739250d080277fc7f1d2449
Mike Cann <mike.cann@gmail.com>
2021-01-28 21:37:35 +0800

    starting work on redirects

--- next.config.js
+++ next.config.js
@@ -1,5 +1,23 @@
+const fs = require("fs");
+const { join } = require("path");
+const matter = require("gray-matter");
+
+const postsDirectory = join(process.cwd(), "public/posts");
+
 module.exports = {
   async rewrites() {
+    // for (let filename of fs.readdirSync(postsDirectory)) {
+    //   const postDir = join(postsDirectory, filename);
+    //   const absPostPath = join(postDir, `post.md`);
+    //   const fileContents = fs.readFileSync(absPostPath, "utf8");
+
+    //   const { data, content } = matter(fileContents);
+
+    //   if (!data.oldUrl) continue;
+
+    //   console.log(data.oldUrl);
+    // }
+
     return [
       {
         source: "/wp-content/:splat*",
